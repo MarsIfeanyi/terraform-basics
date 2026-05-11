aff1cdc300413a4899e3dfb544f7e7f89a3d2e89
Marcellus Ifeanyi <marschalice@gmail.com>
2026-05-11 19:04:33 +0000

    Add gcs bucket name and storage class variable

--- terrabasics/variables.tf
+++ terrabasics/variables.tf
@@ -1,5 +1,17 @@
 variable "bq_dataset_name" {
-    description = "My Bigquery dataset name"
+    description = "My BigQuery Dataset Name"
     default = "mydemo_dataset"
+  
+}
+
+variable "gcs_bucket_name" {
+    description = "My Storage Bucket Name"
+    default = "grand-verve-368213-terrabucket"
+  
+}
+
+variable "gcs_storage_class" {
+    description = "Bucket Storage Class"
+    default = "STANDARD"
   
 }
