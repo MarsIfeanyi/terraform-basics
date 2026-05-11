73947d247bcd52d256546cbfbd3beeb025fb392a
Marcellus Ifeanyi <marschalice@gmail.com>
2026-05-11 19:33:08 +0000

    Create more terraform variables

--- terrabasics/variables.tf
+++ terrabasics/variables.tf
@@ -1,3 +1,31 @@
+
+
+variable "credentials" {
+    description = "My Credentials"
+    default = "./keys/my-creds.json"
+  
+}
+
+
+variable "project" {
+    description = "Project"
+    default = "grand-verve-368213"
+  
+}
+
+variable "region" {
+    description = "Region"
+    default =  "us-central1"
+  
+}
+
+
+variable "location" {
+    description = "Projection Location"
+    default = "US"
+  
+}
+
 variable "bq_dataset_name" {
     description = "My BigQuery Dataset Name"
     default = "mydemo_dataset"
